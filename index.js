b0fa3be1b85f1ec6aa27e6e841955116dbfb8452
sahil bhatia <sahilbhatia220@gmail.com>
2024-10-14 21:25:35 +0530

    3 Allow the add method to handle new lines between numbers (instead of commas)

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function add(numbers) {
         return 0;
     }
 
+    // Replace new lines with commas to handle both separators
+    numbers = numbers.replace(/\n/g, ",");
+
     let numArray = numbers.split(",");
 
 
@@ -25,4 +28,7 @@ function add(numbers) {
 // console.log(add("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,56,57,57,58,59,60,61,62,63,64,65,66,67,68,69,70"));    
 
 let numbers = [...Array(1000000).keys()].map(i => i + 1).join(',');
-console.log(add(numbers)) 
+// console.log(add(numbers)) 
+
+// 3 Allow the add method to handle new lines between numbers (instead of commas)
+console.log(add("1\n2,3"));
